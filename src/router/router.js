8205014091b12779f30f865852884a19c4392474
丶祈梦 <1694081882@qq.com>
2019-11-14 19:56:36 +0800

    添加异步加载

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import login from '../views/login/'
-import home from '../views/home/'
-import notLogged from '../views/notLogged/'
-import mine from '../views/mine/'
-import tabbar from '../views/tabbar_layout/'
-import search from '../views/search/'
-import list from '../views/list/'
-import artDetail from '../views/artDetail/'
-import profile from '../views/profile/'
-import xiaozhi from '../views/xiaozhi/'
+// import login from '../views/login/'
+// import home from '../views/home/'
+// import notLogged from '../views/notLogged/'
+// import mine from '../views/mine/'
+// import tabbar from '../views/tabbar_layout/'
+// import search from '../views/search/'
+// import list from '../views/list/'
+// import artDetail from '../views/artDetail/'
+// import profile from '../views/profile/'
+// import xiaozhi from '../views/xiaozhi/'
+// 异步加载
+const login = () => import('../views/login/')
+const home = () => import('../views/home/')
+const notLogged = () => import('../views/notLogged/')
+const mine = () => import('../views/mine/')
+const tabbar = () => import('../views/tabbar_layout/')
+const search = () => import('../views/search/')
+const list = () => import('../views/list/')
+const artDetail = () => import('../views/artDetail/')
+const profile = () => import('../views/profile/')
+const xiaozhi = () => import('../views/xiaozhi/')
 
 Vue.use(Router)
 
